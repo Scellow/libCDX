1bf260ad56ff74b97e9ea86e6562290a5564f0df
Scellow <scellow33@gmail.com>
2018-07-04 17:53:28 +0200

    Update demo

--- src/CDX.GLFW.Demo/Program.cs
+++ src/CDX.GLFW.Demo/Program.cs
@@ -17,175 +17,29 @@ namespace CDX.GLFWBackend.Demo
         }
     }
 
-    public class Sandbox : IApplicationListener
+    public class Sandbox : ApplicationAdapter
     {
-        public ShaderProgram shader;
-        public SpriteBatch   batch;
-        public Mesh          mesh;
-        public Texture       texture;
-        public OrthographicCamera camera;
+        private SpriteBatch   _batch;
+        private Texture       _texture;
 
-        public void create()
+        public override void create()
         {
             Gdx.app.log("Game", "Create");
             Gdx.input.setInputProcessor(new InputController());
 
-            mesh = new Mesh(Mesh.VertexDataType.VertexBufferObjectWithVAO, false, 1 * 4, 1 * 6,
-                new VertexAttribute(VertexAttributes.Usage.Position, 2, ShaderProgram.POSITION_ATTRIBUTE),
-                new VertexAttribute(VertexAttributes.Usage.ColorPacked, 4, ShaderProgram.COLOR_ATTRIBUTE));
-
-            batch = new SpriteBatch(6000);
-
-            texture = Texture.loadFromFile("badlogic.jpg");
-
-            shader = createDefaultShader();
-
-            mesh.setIndices(new uint[]
-            {
-                0, 1, 3, // first triangle
-                1, 2, 3  // second triangle
-            });
-
-            mesh.setVertices(new float[]
-            {
-                50f, 50f,255, 255, 0, 255, // top right
-                50f, 0f, 255, 255, 0, 255,  // bottom right
-                0f, 0f,  255, 255, 0, 255,   // bottom left
-                0f, 50f, 255, 255, 0, 255,  // top left 
-            });
-            
-            camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+            _batch = new SpriteBatch();
+            _texture = Texture.loadFromFile("badlogic.jpg");
         }
 
-        public void resize(int width, int height)
-        {
-            Gdx.app.log("Game", $"Resize: {width}:{height}");
-        }
-
-        public void render()
+        public override void render()
         {
             GL.Viewport(0,0, Gdx.graphics.getBackBufferWidth(), Gdx.graphics.getHeight());
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
             GL.ClearColor(0.5f,0,0.25f, 1);
             
-            
-            //batch.setProjectionMatrix(camera.combined);
-            batch.begin();
-            batch.draw(texture, 0, 0);
-            batch.end();
-            
-            
-        }
-
-        public void pause()
-        {
-            Gdx.app.log("Game", "Pause");
-        }
-
-        public void resume()
-        {
-            Gdx.app.log("Game", "Resume");
-        }
-
-        public void dispose()
-        {
-            Gdx.app.log("Game", "Dispose");
-        }
-
-        public static ShaderProgram createDefaultShader()
-        {
-            var vertexShader =   "attribute vec4 " + ShaderProgram.POSITION_ATTRIBUTE + ";\n"                    //
-                               + "attribute vec4 " + ShaderProgram.COLOR_ATTRIBUTE + ";\n"                     //
-                               + "uniform mat4 u_projTrans;\n"                                                 //
-                               + "varying vec4 v_color;\n"                                                     //
-                               + "\n"                                                                          //
-                               + "void main()\n"                                                               //
-                               + "{\n"                                                                         //
-                               + "   v_color = " + ShaderProgram.COLOR_ATTRIBUTE + ";\n"                       //
-                               + "   v_color.a = v_color.a * (255.0/254.0);\n"                                 //
-                               + "   gl_Position =  u_projTrans * " + ShaderProgram.POSITION_ATTRIBUTE + ";\n" //
-                               + "}\n";
-
-
-            var fragmentShader =  "varying vec4 v_color;\n"  //
-                                 + "void main()\n"               //
-                                 + "{\n"                         //
-                                 + "  gl_FragColor = v_color;\n" //
-                                 + "}";
-
-            var shader = new ShaderProgram(vertexShader, fragmentShader);
-            if (shader.isCompiled_() == false) throw new Exception("Error compiling shader: " + shader.getLog());
-            return shader;
-        }
-
-        public static (Mesh, ShaderProgram) createMesh()
-        {
-            var mesh = new Mesh(true, 6, 0,
-                new VertexAttributes(
-                    new VertexAttribute(VertexAttributes.Usage.Position, 3, ShaderProgram.POSITION_ATTRIBUTE),
-                    new VertexAttribute(VertexAttributes.Usage.TextureCoordinates, 2, ShaderProgram.TEXCOORD_ATTRIBUTE + "0")
-                    )
-            );
-
-
-            var   verts = new float[30];
-            var   i     = 0;
-            float x,     y;      // Mesh location in the world
-            float width, height; // Mesh width and height
-
-            x     = y      = 50f;
-            width = height = 300f;
-
-            //Top Left Vertex Triangle 1
-            verts[i++] = x;          //X
-            verts[i++] = y + height; //Y
-            verts[i++] = 0;          //Z
-            verts[i++] = 0f;         //U
-            verts[i++] = 0f;         //V
-
-            //Top Right Vertex Triangle 1
-            verts[i++] = x + width;
-            verts[i++] = y + height;
-            verts[i++] = 0;
-            verts[i++] = 1f;
-            verts[i++] = 0f;
-
-            //Bottom Left Vertex Triangle 1
-            verts[i++] = x;
-            verts[i++] = y;
-            verts[i++] = 0;
-            verts[i++] = 0f;
-            verts[i++] = 1f;
-
-            //Top Right Vertex Triangle 2
-            verts[i++] = x + width;
-            verts[i++] = y + height;
-            verts[i++] = 0;
-            verts[i++] = 1f;
-            verts[i++] = 0f;
-
-            //Bottom Right Vertex Triangle 2
-            verts[i++] = x + width;
-            verts[i++] = y;
-            verts[i++] = 0;
-            verts[i++] = 1f;
-            verts[i++] = 1f;
-
-            //Bottom Left Vertex Triangle 2
-            verts[i++] = x;
-            verts[i++] = y;
-            verts[i++] = 0;
-            verts[i++] = 0f;
-            verts[i]   = 1f;
-            
-            mesh.setVertices(verts);
-
-            var shaderProgram = new ShaderProgram(
-                File.ReadAllText("shader.vert"),
-                File.ReadAllText("shader.frag")
-            );
-
-            return (mesh, shaderProgram);
+            _batch.begin();
+            _batch.draw(_texture, 0, 0);
+            _batch.end();
         }
     }
 
